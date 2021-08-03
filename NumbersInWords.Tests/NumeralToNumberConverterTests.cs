5dd079416546e3392cff96eb91ddf979d7335348
Jack Kendall <jkendall3096@gmail.com>
2021-08-03 13:01:08 +0100

    Create numeral-to-number converter. Original commits:
    Simplify LINQ
    
    Renaming
    
    Encapsulate method
    
    Catch integer overflow
    
    Simplify minus handling
    
    Better working example
    
    Halfway working

--- NumbersInWords.Tests/NumeralToNumberConverterTests.cs
+++ NumbersInWords.Tests/NumeralToNumberConverterTests.cs
@@ -9,13 +9,13 @@ namespace NumbersInWords.Tests
 
         [InlineData("zero", 0)]
         [InlineData("one", 1)]
+        [InlineData("twenty-three", 23)]
         [InlineData("minus twenty-three", -23)]
-        [InlineData("two hundred and thirty-four million six hundred and fifty-four", 234_000_654)]
         [InlineData("four hundred and fifty-four", 454)]
-        [InlineData("two thousand one hundred and forty-seven million four hundred and eighty-three thousand six hundred and forty-seven", int.MaxValue)]
-        [InlineData("minus two thousand one hundred and forty-seven million four hundred and eighty-three thousand six hundred and forty-seven", int.MinValue)]
-        [InlineData("three thousand one hundred and forty-seven million four hundred and eighty-three thousand six hundred and forty-seven", int.MaxValue)] // overflow
-        [InlineData("minus three thousand one hundred and forty-seven million four hundred and eighty-three thousand six hundred and forty-seven", int.MinValue)] // negative overflow
+        [InlineData("one thousand", 1000)]
+        [InlineData("two hundred and thirty-four million six hundred and fifty-four", 234_000_654)]
+        [InlineData("three billion two hundred and thirty-four million six hundred and fifty-four", int.MaxValue)]
+        [InlineData("minus three billion two hundred and thirty-four million six hundred and fifty-four", int.MinValue)]
         [Theory]
         public void ToNumbers_ShouldReturnCorrectNumber_ForSingleValue(string value, int expected)
         {
--- NumbersInWords/NumeralToNumberConverter.cs
+++ NumbersInWords/NumeralToNumberConverter.cs
@@ -1,18 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace NumbersInWords
 {
     public class NumeralToNumberConverter
     {
+        private readonly Dictionary<string, int> _numbers = new()
+        {
+            { "zero", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+            { "ten", 10 },
+
+            { "eleven", 11 },
+            { "twelve", 12 },
+            { "thirteen", 13 },
+            { "fourteen", 14 },
+            { "fifteen", 15 },
+            { "sixteen", 16 },
+            { "seventeen", 17 },
+            { "eighteen", 18 },
+            { "nineteen", 19 },
+
+            { "twenty", 20 },
+            { "thirty", 30 },
+            { "forty", 40 },
+            { "fifty", 50 },
+            { "sixty", 60 },
+            { "seventy", 70 },
+            { "eighty", 80 },
+            { "ninety", 90 },
+
+            { "hundred", 100 },
+            { "thousand", 1000 },
+            { "million", 1000000 },
+            { "billion", 1000000000 }
+        };
+
         /// <summary>
         /// Converts a numeral to an integer. Invalid input is silently omitted.
         /// </summary>
         /// <param name="values">The numeral to convert.</param>
         /// <returns>The converted value.</returns>
-        public int ToNumbers(string value)
+        public int ToNumbers(string input)
         {
-            throw new NotImplementedException();
+            input = input.ToLowerInvariant();
+
+            var numbers = Regex.Matches(input, @"\w+")
+                .Where(v => _numbers.ContainsKey(v.Value))
+                .Select(v => _numbers[v.Value]);
+
+            bool isNegative = input.StartsWith("minus", StringComparison.InvariantCultureIgnoreCase);
+
+            var result = Accumulate(numbers, isNegative);
+
+            if (isNegative)
+                result *= -1;
+
+            return result;
+        }
+
+        private int Accumulate(IEnumerable<int> numbers, bool negativeNumber)
+        {
+            int accumulator = 0;
+            int total = 0;
+
+            try
+            {
+                checked
+                {
+                    foreach (var n in numbers)
+                    {
+                        if (n >= 1000)
+                        {
+                            total += (accumulator * n);
+                            accumulator = 0;
+                        }
+                        else if (n >= 100)
+                        {
+                            accumulator *= n;
+                        }
+                        else
+                        {
+                            accumulator += n;
+                        }
+                    }
+                }
+
+            }
+            catch (OverflowException)
+            {
+                return negativeNumber ? int.MinValue : int.MaxValue;
+            }
+
+            return total + accumulator;
         }
 
         /// <summary>
